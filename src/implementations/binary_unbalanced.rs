06ba1ca582116375f653b9b8537d654d2bff2c83
Oliver Calder <oliver@calder.dev>
2025-06-21 23:52:32 -0500

    Added documentation and examples with doc tests
    Signed-off-by: Oliver Calder <oliver@calder.dev>

--- src/implementations/binary_unbalanced.rs
+++ src/implementations/binary_unbalanced.rs
@@ -18,6 +18,19 @@ impl<T> NodeBinaryUnbalanced<T>
 where
     T: fmt::Display + Ord,
 {
+    /// Returns a new node with the given value.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use simple_tree::implementations::NodeBinaryUnbalanced;
+    /// use simple_tree::Node;
+    ///
+    /// let root = NodeBinaryUnbalanced::new("foo");
+    ///
+    /// assert_eq!(root.value(), &"foo");
+    /// assert_eq!(root.count_children(), 0);
+    /// ```
     pub fn new(value: T) -> Self {
         NodeBinaryUnbalanced {
             val: value,
@@ -27,6 +40,36 @@ where
         }
     }
 
+    /// Creates a new node with the given value and adds it to the tree under self.
+    /// If the new value is less than the value of self, inserts the new node as the left child of
+    /// self if none exists, otherwise calls `insert(value)` on the existing left child.
+    ///
+    /// If the new value is greater than the value of self, inserts the new node as the right child
+    /// of self if none exists, otherwise calls `insert(value)` on the existing right child.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use simple_tree::implementations::NodeBinaryUnbalanced;
+    /// use simple_tree::Node;
+    ///
+    /// let mut root = NodeBinaryUnbalanced::new(7);
+    /// root.insert(3);
+    /// root.insert(5);
+    /// root.insert(13);
+    /// root.insert(2);
+    /// root.insert(11);
+    /// root.insert(15);
+    ///
+    /// assert_eq!(format!("{}", root), "
+    /// 7
+    /// ├── 3
+    /// │   ├── 2
+    /// │   └── 5
+    /// └── 13
+    ///     ├── 11
+    ///     └── 15");
+    /// ```
     pub fn insert(&mut self, value: T) {
         match value.cmp(&self.val) {
             Ordering::Less => {
@@ -52,10 +95,42 @@ impl<T> Node<T> for NodeBinaryUnbalanced<T>
 where
     T: fmt::Display + Ord,
 {
+    /// Returns the value stored in [self].
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use simple_tree::implementations::NodeBinaryUnbalanced;
+    /// use simple_tree::Node;
+    ///
+    /// let root = NodeBinaryUnbalanced::new("hello");
+    ///
+    /// assert_eq!(root.value(), &"hello");
+    /// assert_eq!(root.children().count(), 0);
+    /// ```
     fn value(&self) -> &T {
         &self.val
     }
 
+    /// Returns an iterator which yields the left child, if present, and then the right child, if
+    /// present.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use simple_tree::implementations::NodeBinaryUnbalanced;
+    /// use simple_tree::Node;
+    ///
+    /// let mut root = NodeBinaryUnbalanced::new(50);
+    /// root.insert(25);
+    /// root.insert(100);
+    /// root.insert(75);
+    ///
+    /// let mut children = root.children();
+    /// assert_eq!(children.next().unwrap().value(), &25);
+    /// assert_eq!(children.next().unwrap().value(), &100);
+    /// assert_eq!(children.next().is_none(), true);
+    /// ```
     fn children(&self) -> impl Iterator<Item = &Box<Self>> {
         let mut ch = Vec::new();
         if let Some(left) = &self.left {
@@ -72,6 +147,7 @@ impl<T> fmt::Display for NodeBinaryUnbalanced<T>
 where
     T: fmt::Display + Ord,
 {
+    /// Format using the default [Node::fmt] implementation.
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         Node::fmt(self, f)
     }
--- src/node.rs
+++ src/node.rs
@@ -7,22 +7,125 @@ enum SpecialStatus {
     LastChild,
 }
 
-/// Node defines common methods for different node implementations.
+/// [Node] defines common methods for nodes in a displayable tree.
 pub trait Node<T>
 where
     T: fmt::Display,
 {
+    /// Returns the displayable value represented by this node.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use simple_tree::implementations::NodeBinaryUnbalanced;
+    /// use simple_tree::Node;
+    ///
+    /// let root = NodeBinaryUnbalanced::new("hello world!");
+    ///
+    /// assert_eq!(root.value(), &"hello world!");
+    /// ```
     fn value(&self) -> &T;
+
+    /// Returns an iterator over the children of this node. Children of those children must not be
+    /// included.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use simple_tree::implementations::NodeBinaryUnbalanced;
+    /// use simple_tree::Node;
+    ///
+    /// let mut root = NodeBinaryUnbalanced::new(50);
+    /// root.insert(25);
+    /// root.insert(100);
+    /// root.insert(75);
+    ///
+    /// let mut children = root.children();
+    /// assert_eq!(children.next().unwrap().value(), &25);
+    /// assert_eq!(children.next().unwrap().value(), &100);
+    /// assert_eq!(children.next().is_none(), true);
+    /// ```
     fn children(&self) -> impl Iterator<Item = &Box<Self>>;
 
+    /// Returns the number of direct children of this node.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use simple_tree::implementations::NodeBinaryUnbalanced;
+    /// use simple_tree::Node;
+    ///
+    /// let mut root = NodeBinaryUnbalanced::new(50);
+    /// root.insert(25);
+    /// root.insert(100);
+    /// root.insert(75);
+    ///
+    /// assert_eq!(root.count_children(), 2);
+    /// let mut children = root.children();
+    ///
+    /// let first = children.next().unwrap();
+    /// assert_eq!(first.count_children(), 0);
+    ///
+    /// let second = children.next().unwrap();
+    /// assert_eq!(second.count_children(), 1);
+    /// ```
     fn count_children(&self) -> usize {
         self.children().count()
     }
 
+    /// Returns the number of children of this node, and each of their children, recursively.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use simple_tree::implementations::NodeBinaryUnbalanced;
+    /// use simple_tree::Node;
+    ///
+    /// let mut root = NodeBinaryUnbalanced::new(50);
+    /// root.insert(25);
+    /// root.insert(100);
+    /// root.insert(75);
+    ///
+    /// assert_eq!(root.count_descendents(), 3);
+    /// ```
     fn count_descendents(&self) -> usize {
         self.children().map(|c| c.count_descendents() + 1).sum()
     }
 
+    /// Formats a tree rooted at the given node and writes the result to the given formatter.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use simple_tree::implementations::NodeBinaryUnbalanced;
+    /// use simple_tree::Node;
+    ///
+    /// let mut root = NodeBinaryUnbalanced::new(7);
+    /// root.insert(3);
+    /// root.insert(5);
+    /// root.insert(13);
+    /// root.insert(2);
+    /// root.insert(11);
+    /// root.insert(15);
+    ///
+    /// assert_eq!(format!("{}", root), "
+    /// 7
+    /// ├── 3
+    /// │   ├── 2
+    /// │   └── 5
+    /// └── 13
+    ///     ├── 11
+    ///     └── 15");
+    /// ```
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut prefixes = vec!["\n"];
+        self.print_tree(f, &mut prefixes, SpecialStatus::Root)
+    }
+
+    /// A helper method of [Self::fmt] which recursively prints a node and its children to the given
+    /// formatter, given some existing prefixes and metadata about this node.
+    ///
+    /// This should never be called directly.
     fn print_tree(
         &self,
         f: &mut fmt::Formatter<'_>,
@@ -33,7 +136,7 @@ where
             write!(f, "{}", pref)?;
         }
         let (s_prefix, c_prefix) = match status {
-            SpecialStatus::None => ("├── ", "│   "),
+            SpecialStatus::None => ("├── ", "│   "),
             SpecialStatus::Root => ("", ""),
             SpecialStatus::LastChild => ("└── ", "    "),
         };
@@ -50,10 +153,5 @@ where
         }
         let _ = prefixes.pop();
         Ok(())
-    }
-
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let mut prefixes = vec!["\n"];
-        self.print_tree(f, &mut prefixes, SpecialStatus::Root)
     }
 }
