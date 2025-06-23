436f0bebd90083ff7bebea396bd9379b0a1046ae
Oliver Calder <oliver@calder.dev>
2025-06-23 00:05:14 -0500

    Add trie implementation
    Signed-off-by: Oliver Calder <oliver@calder.dev>

--- src/implementations.rs
+++ src/implementations.rs
@@ -1,5 +1,7 @@
 pub mod binary_unbalanced;
 pub mod dir_tree;
+pub mod trie;
 
 pub use binary_unbalanced::NodeBinaryUnbalanced;
 pub use dir_tree::DirTree;
+pub use trie::Trie;
---
+++ src/implementations/trie.rs
@@ -0,0 +1,233 @@
+use std::collections::BTreeMap;
+use std::fmt;
+
+use crate::node::Node;
+
+pub struct Trie {
+    // The word or partial word associated with this node.
+    fragment: String,
+    // The number of times exactly this word has occurred.
+    count: usize,
+    // The nodes of other words which begin with this node's fragment.
+    children: BTreeMap<char, Trie>,
+}
+
+impl Trie {
+    /// Returns an empty [Trie].
+    pub fn new() -> Self {
+        Self::new_with_fragment(String::new())
+    }
+
+    /// Returns a new [Trie] node with the given word fragment and a count of `0`.
+    fn new_with_fragment(fragment: String) -> Self {
+        Trie {
+            fragment,
+            count: 0,
+            children: BTreeMap::new(),
+        }
+    }
+
+    /// Returns a new [Trie] populated with the given words.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use simple_tree::implementations::Trie;
+    /// use simple_tree::Node;
+    ///
+    /// let trie = Trie::from(vec!["foo", "bar", "baz", "foo", "baz", "foo", "b"]);
+    ///
+    /// assert_eq!(format!("{}", trie), "
+    /// ├── b\t1
+    /// │   └── ba\t0
+    /// │       ├── bar\t1
+    /// │       └── baz\t2
+    /// └── f\t0
+    ///     └── fo\t0
+    ///         └── foo\t3");
+    /// ```
+    pub fn from<'a>(words: impl IntoIterator<Item = &'a str>) -> Self {
+        words.into_iter().fold(Self::new(), |mut acc, w| {
+            acc.add(w.to_string()).unwrap(); // error cannot occur
+            acc
+        })
+    }
+
+    /// Returns the next `char` in the given word following `self.fragment`, if it exists.
+    fn next_char(&self, word: &str) -> Option<char> {
+        word.chars().nth(self.fragment.len())
+    }
+
+    /// Returns the [Trie] node corresponding to the next `char` in the given word following
+    /// `self.fragment`, if there is a next letter and the corresponding node exists.
+    fn next_node(&self, word: &str) -> Option<&Trie> {
+        self.children.get(&self.next_char(word)?)
+    }
+
+    /// Returns the [Trie] node corresponding to the next `char` in the given word following
+    /// `self.fragment`, if there is a next letter and the corresponding node exists.
+    fn next_node_mut(&mut self, word: &str) -> Option<&mut Trie> {
+        self.children.get_mut(&self.next_char(word)?)
+    }
+
+    /// Returns a reference to the [Trie] node corresponding to the given word fragment, if it
+    /// exists in the (sub)trie rooted at `self`.
+    fn get(&self, fragment: &str) -> Option<&Trie> {
+        if fragment == self.fragment {
+            return Some(self);
+        }
+        self.next_node(fragment)?.get(fragment)
+    }
+
+    /// Returns a mutable reference to the [Trie] node corresponding to the given word fragment, if
+    /// it exists in the (sub)trie rooted at `self`.
+    fn get_mut(&mut self, fragment: &str) -> Option<&mut Trie> {
+        if fragment == self.fragment {
+            return Some(self);
+        }
+        self.next_node_mut(fragment)?.get_mut(fragment)
+    }
+
+    /// Returns the number of times the given word has been added to the [Trie].
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use simple_tree::implementations::Trie;
+    /// use simple_tree::Node;
+    ///
+    /// let trie = Trie::from(vec!["foo", "bar", "baz", "foo", "baz", "foo", "b"]);
+    /// assert_eq!(trie.occurrences("foo"), 3);
+    /// assert_eq!(trie.occurrences("bar"), 1);
+    /// assert_eq!(trie.occurrences("baz"), 2);
+    /// assert_eq!(trie.occurrences("b"), 1);
+    ///
+    /// assert_eq!(trie.occurrences("f"), 0);
+    /// assert_eq!(trie.occurrences("fo"), 0);
+    /// assert_eq!(trie.occurrences("ba"), 0);
+    /// ```
+    pub fn occurrences(&self, word: &str) -> usize {
+        self.get(word).map_or(0, |n| n.count)
+    }
+
+    /// Returns an iterator over the potential next characters following the given word fragment
+    /// which are currently present in the [Trie].
+    pub fn next_chars(&self, fragment: &str) -> impl Iterator<Item = &char> {
+        self.get(fragment)
+            .map_or_else(Default::default, |n| n.children.keys())
+    }
+
+    /// Adds the given word to the [Trie] and returns the total number of times it now occurs.
+    ///
+    /// If the given word is already in the trie, then increment its occurrences count by 1.
+    /// Otherwise, add nodes to the trie for the word and any prefixes which are not yet present in
+    /// the trie, and set the occurrence count of the new node for the word itself to be 1.
+    ///
+    /// Examples
+    ///
+    /// ```
+    /// use simple_tree::implementations::Trie;
+    /// use simple_tree::Node;
+    ///
+    /// let mut trie = Trie::new();
+    ///
+    /// assert_eq!(trie.add(String::from("hello")), Ok(1));
+    /// assert_eq!(trie.add(String::from("hi")), Ok(1));
+    /// assert_eq!(trie.add(String::from("hey")), Ok(1));
+    /// assert_eq!(trie.add(String::from("hi")), Ok(2));
+    ///
+    /// assert_eq!(format!("{}", trie), "
+    /// └── h\t0
+    ///     ├── he\t0
+    ///     │   ├── hel\t0
+    ///     │   │   └── hell\t0
+    ///     │   │       └── hello\t1
+    ///     │   └── hey\t1
+    ///     └── hi\t2")
+    /// ```
+    pub fn add(&mut self, word: String) -> Result<usize, &'static str> {
+        if !word.starts_with(&self.fragment) {
+            return Err("cannot add word to node with incompatible prefix");
+        }
+        let Some(next_char) = self.next_char(&word) else {
+            self.count += 1;
+            return Ok(self.count);
+        };
+        Ok(self
+            .children
+            .entry(next_char)
+            .or_insert_with(|| Self::new_with_fragment(word[..self.fragment.len() + 1].to_string()))
+            .add(word)
+            .unwrap())
+    }
+}
+
+impl Node for Trie {
+    /// Returns a displayable representation of this trie node, which includes the word or fragment
+    /// associated with the node along with the count of the number of times that exact word has
+    /// occurred (which may be 0).
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use simple_tree::implementations::Trie;
+    /// use simple_tree::Node;
+    ///
+    /// let root = Trie::from(vec!["foo", "bar", "baz"]);
+    /// assert_eq!(format!("{}", root.value()), "");
+    ///
+    /// let b_node = root.children().next().unwrap();
+    /// assert_eq!(format!("{}", b_node.value()), "b\t0");
+    ///
+    /// let ba_node = b_node.children().next().unwrap();
+    /// assert_eq!(format!("{}", ba_node.value()), "ba\t0");
+    ///
+    /// let bar_node = ba_node.children().next().unwrap();
+    /// assert_eq!(format!("{}", bar_node.value()), "bar\t1");
+    /// ```
+    fn value(&self) -> impl fmt::Display {
+        if self.fragment.is_empty() {
+            return String::new();
+        }
+        format!("{}\t{}", &self.fragment, self.count)
+    }
+
+    /// Returns an iterator over the [Trie] nodes whose
+    /// associated with the node along with the count of the number of times that exact word has
+    /// occurred (which may be 0).
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use simple_tree::implementations::Trie;
+    /// use simple_tree::Node;
+    ///
+    /// let root = Trie::from(vec!["foo", "bar", "baz"]);
+    /// assert_eq!(format!("{}", root.value()), "");
+    ///
+    /// let b_node = root.children().next().unwrap();
+    /// assert_eq!(format!("{}", b_node.value()), "b\t0");
+    ///
+    /// let ba_node = b_node.children().next().unwrap();
+    /// assert_eq!(format!("{}", ba_node.value()), "ba\t0");
+    ///
+    /// let bar_node = ba_node.children().next().unwrap();
+    /// assert_eq!(format!("{}", bar_node.value()), "bar\t1");
+    /// ```
+    fn children(&self) -> impl Iterator<Item = &Self> {
+        self.children.values()
+    }
+}
+
+impl fmt::Display for Trie {
+    /// Format using the default [Node::fmt] implementation.
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        Node::fmt(self, f)
+    }
+}
+
+impl Default for Trie {
+    fn default() -> Self {
+        Self::new()
+    }
+}
